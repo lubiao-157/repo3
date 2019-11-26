962456bf03213e6c08a8a4d4eddf2a858baa04f2
chenli <chenli@qq.com>
2019-11-26 15:40:23 +0800

    第一次提交

---
+++ test/src/com/kgc/demo/test.java
@@ -0,0 +1,10 @@
+package com.kgc.demo;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("qqqq");
+        System.out.println("qqqq");
+        System.out.println("qqqq");
+        System.out.println("qqqq");
+    }
+}
